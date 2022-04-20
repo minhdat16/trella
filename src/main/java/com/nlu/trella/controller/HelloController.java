a2ed512772da8f2c1ea964d21da0542f12d6cf80
= <=>
2022-04-20 21:53:13 +0700

    set env

--- src/main/java/com/nlu/trella/controller/HelloController.java
+++ src/main/java/com/nlu/trella/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.nlu.trella.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping(value = "/hello")
+    public String greet(){
+        return "Trella say hello!";
+    }
 }
