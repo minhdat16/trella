d319b255c661d5f865e552f1f4a094fea935fc29
= <=>
2022-04-20 21:16:52 +0700

    first commit

---
+++ src/main/java/com/nlu/clonetrello/ClonetrelloApplication.java
@@ -0,0 +1,13 @@
+package com.nlu.clonetrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonetrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonetrelloApplication.class, args);
+	}
+
+}
